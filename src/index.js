6c9cf82c0e58600e0612e02d12febca406321c6b
nastaran7911 <nastaran7911@gmail.com>
2025-03-25 13:52:38 +0330

    Added the API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-function generateEmail(event) {
-  event.preventDefault();
+function displayEmail(response) {
+  console.log("email generated");
   new Typewriter("#email", {
-    strings: "The Email will be here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateEmail(event) {
+  event.preventDefault();
+  let instructionInput = document.querySelector("#user-instructions");
+  let apiKey = "179d18a7b4e4aebt8ff9c850b9e3060o";
+
+  let context =
+    "You are a professional mail writer and love to write Emails. Your mission is a 6 line Email in basic HTML. Make sure to follow the user instruction below";
+  let prompt = ` User instructions: Generate an Email about ${instructionInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayEmail);
+  console.log("generating email");
 }
 
 let emailFormElement = document.querySelector("#email-generator");
